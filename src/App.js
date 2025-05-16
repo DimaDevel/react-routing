640397a7246d5e04a886d6be29bbfee64ff4dbc2
Dima <dima.ivanov.dev@gmail.com>
2025-05-16 10:19:59 +0200

    Added index in the router.

--- src/App.js
+++ src/App.js
@@ -23,9 +23,10 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/", element: <HomePage /> },
-      { path: "/products", element: <ProductsPage /> },
-      { path: "/products/:productId", element: <ProductDetailPage /> },
+      { index: true, element: <HomePage /> },
+      // { path: "", element: <HomePage /> },
+      { path: "products", element: <ProductsPage /> },
+      { path: "products/:productId", element: <ProductDetailPage /> },
     ],
   },
 ]);
